f88963f7f12379b9317f12fef2d1c6931ca5068c
Man7 <vii88u@gmail.com>
2026-01-29 00:28:11 +0800

    Update main.js
    glados.rocks 替换为 glados.cloud

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const glados = async () => {
   try {
     const headers = {
       'cookie': cookie,
-      'referer': 'https://glados.rocks/console/checkin',
+      'referer': 'https://glados.cloud/console/checkin',
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
     }
-    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
+    const checkin = await fetch('https://glados.cloud/api/user/checkin', {
       method: 'POST',
       headers: { ...headers, 'content-type': 'application/json' },
       body: '{"token":"glados.one"}',
     }).then((r) => r.json())
-    const status = await fetch('https://glados.rocks/api/user/status', {
+    const status = await fetch('https://glados.cloud/api/user/status', {
       method: 'GET',
       headers,
     }).then((r) => r.json())
