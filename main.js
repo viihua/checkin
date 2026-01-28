849bfe159739dd1920df4c789c37287d54dd2cd5
Man7 <vii88u@gmail.com>
2026-01-29 00:43:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const glados = async () => {
     const headers = {
       'cookie': cookie,
       'referer': 'https://glados.cloud/console/checkin',
-      'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
     }
     const checkin = await fetch('https://glados.cloud/api/user/checkin', {
       method: 'POST',
