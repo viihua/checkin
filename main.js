45c0baee636850b22b5a7d765779dc50e14190ad
Man7 <vii88u@gmail.com>
2026-01-29 00:48:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const glados = async () => {
   try {
     const headers = {
       'cookie': cookie,
-      'referer': 'https://glados.cloud/console/checkin',
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
+      'referer': 'https://glados.rocks/console/checkin',
+      'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
     }
-    const checkin = await fetch('https://glados.cloud/api/user/checkin', {
+    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
       method: 'POST',
       headers: { ...headers, 'content-type': 'application/json' },
       body: '{"token":"glados.one"}',
     }).then((r) => r.json())
-    const status = await fetch('https://glados.cloud/api/user/status', {
+    const status = await fetch('https://glados.rocks/api/user/status', {
       method: 'GET',
       headers,
     }).then((r) => r.json())
